516ff811491da8b8a78a9aacfeb5e0d5a90ed357
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-07-10 14:24:10 +0900

    fix: add 23.07.10

--- 23.07.10_event/public/index.js
+++ 23.07.10_event/public/index.js
@@ -74,6 +74,79 @@ window.onresize = function (e) {
 
 console.log("b" + "a" + +"a" + "a");
 
-window.onkeydown = (e) => {
-  console.log(e);
+// window.onkeydown = (e) => {
+//   console.log(e);
+// };
+
+// document.getElementById("test-form").onsubmit = (e) => {
+//   e.preventDefault();
+//   // alert("데이터 보냈어");
+//   console.log("데이터 안보냇어");
+// };
+
+console.log(document.forms["test-form"]);
+// console.log(document.forms.test-form); << 안됨
+
+document.forms["test-form"].onsubmit = (e) => {
+  e.preventDefault();
+  console.log("데이터 입력 완료");
 };
+
+document.forms["test-form"].getElementsByTagName("button")[0].onsubmit = (
+  e
+) => {
+  e.preventDefault();
+  console.log("버튼 서브밋?");
+};
+
+document.body.onload = () => {
+  document.getElementById("test-img").onload = (e) => {
+    console.log(e.target);
+    console.log("이미지 온로드 확인");
+  };
+  document
+    .getElementById("test-img")
+    .setAttribute("src", "public/B002418205-1.jpg");
+};
+
+document.getElementById("name").onfocus = function (e) {
+  console.log("포커싱");
+};
+
+document.getElementById("name").onblur = function (e) {
+  console.log("밖으로 나갔어");
+};
+
+document.getElementById("name").onchange = function (e) {
+  console.log(e.target.value);
+  document.getElementById("name-alert").innerHTML = e.target.value;
+};
+
+document.forms["test-form"]["name"].oninput = function (e) {
+  console.log(e.target.value);
+};
+
+let a = 1;
+// 템플릿 리터널 => string, HTML에서의 pre 같은 형식
+console.log(`
+1 옆에 있는 백틱이다.
+${a * 10} << 여기에 변수를 입력 가능
+${[1, 2, 3, 4]} << 여기에 변수를 입력 가능
+${{ a: 1, b: 2 }} << 여기에 변수를 입력 가능
+`);
+console.log(
+  "1 옆에 있는 백틱이다.\n" + a * 10 + "<< 여기에 변수를 입력 가능\n"
+);
+
+const tempStudent = {
+  name: "김지훈",
+  age: 26,
+};
+
+document.getElementById(
+  "template-literal"
+).innerHTML = `이름은 "${tempStudent.name}"이고 나이는 ${tempStudent.age}입니다.`;
+
+// eval("console.log('테스트')")
+
+// mouseover, mouseenter, mouseout, mouseleave
